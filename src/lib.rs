45a0c6e7a253f9944cb40e3361a11ed31b11bb48
Magic Len <len@magiclen.org>
2018-08-18 18:31:06 +0800

    add "cfg!(not(windows))" for documentation

--- src/lib.rs
+++ src/lib.rs
@@ -35,9 +35,13 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("./path/to/123/456");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!(Path::join(&CWD, Path::new("path/to/123/456")).to_str().unwrap(), p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("./path/to/123/456");
+    ///
+    ///     assert_eq!(Path::join(&CWD, Path::new("path/to/123/456")).to_str().unwrap(), p.parse_dot().unwrap().to_str().unwrap());
+    ///
+    /// }
     /// ```
     ///
     /// If a path starts with a pair of dots, the dots means the parent of **current working directory**. If **current working directory** is **root**, the parent is still **root**.
@@ -49,17 +53,21 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("../path/to/123/456");
+    /// if cfg!(not(windows)) {
     ///
-    /// let cwd_parent = CWD.parent();
+    ///     let p = Path::new("../path/to/123/456");
     ///
-    /// match cwd_parent {
-    ///    Some(cwd_parent) => {
+    ///     let cwd_parent = CWD.parent();
+    ///
+    ///     match cwd_parent {
+    ///        Some(cwd_parent) => {
     ///       assert_eq!(Path::join(&cwd_parent, Path::new("path/to/123/456")).to_str().unwrap(), p.parse_dot().unwrap().to_str().unwrap());
-    ///    }
-    ///    None => {
-    ///       assert_eq!(Path::join(Path::new("/"), Path::new("path/to/123/456")).to_str().unwrap(), p.parse_dot().unwrap().to_str().unwrap());
-    ///    }
+    ///        }
+    ///        None => {
+    ///           assert_eq!(Path::join(Path::new("/"), Path::new("path/to/123/456")).to_str().unwrap(), p.parse_dot().unwrap().to_str().unwrap());
+    ///        }
+    ///     }
+    ///
     /// }
     /// ```
     ///
@@ -72,9 +80,12 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("/path/to/../123/456/./777");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!("/path/123/456/777", p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("/path/to/../123/456/./777");
+    ///
+    ///     assert_eq!("/path/123/456/777", p.parse_dot().unwrap().to_str().unwrap());
+    /// }
     /// ```
     ///
     /// ```
@@ -84,9 +95,13 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("/path/to/../123/456/./777/..");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!("/path/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("/path/to/../123/456/./777/..");
+    ///
+    ///     assert_eq!("/path/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///
+    /// }
     /// ```
     ///
     /// You should notice that `parse_dot` method does **not** aim to get an **absolute path**. A path which does not start with a `MAIN_SEPARATOR`, **Single Dot** and **Double Dots**, will not have each of them after the `parse_dot` method is used.
@@ -98,9 +113,13 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("path/to/../123/456/./777/..");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!("path/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("path/to/../123/456/./777/..");
+    ///
+    ///     assert_eq!("path/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///
+    /// }
     /// ```
     ///
     /// **Double Dots** which is not placed at the start cannot get the parent beyond the original path. Why not? With this constraint, you can insert an absolute path to the start as a virtual root in order to protect your file system from being exposed.
@@ -112,9 +131,13 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("path/to/../../../../123/456/./777/..");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!("123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("path/to/../../../../123/456/./777/..");
+    ///
+    ///     assert_eq!("123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///
+    /// }
     /// ```
     ///
     /// ```
@@ -124,9 +147,13 @@ pub trait ParseDot {
     ///
     /// use path_dedot::*;
     ///
-    /// let p = Path::new("/path/to/../../../../123/456/./777/..");
+    /// if cfg!(not(windows)) {
     ///
-    /// assert_eq!("/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///     let p = Path::new("/path/to/../../../../123/456/./777/..");
+    ///
+    ///     assert_eq!("/123/456", p.parse_dot().unwrap().to_str().unwrap());
+    ///
+    /// }
     /// ```
     fn parse_dot(&self) -> io::Result<PathBuf>;
 }
