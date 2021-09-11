e95ca644271a66e0f6c7cdc0bfad73c2298aa657
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-09-11 08:37:18 -0500

    feat: support giving cwd as an argument

--- src/parse_dot.rs
+++ src/parse_dot.rs
@@ -6,4 +6,7 @@ use std::path::Path;
 pub trait ParseDot {
     /// Remove dots in the path and create a new `PathBuf` instance on demand.
     fn parse_dot(&self) -> io::Result<Cow<Path>>;
+
+    /// Remove dots in the path and create a new `PathBuf` instance on demand. It gets the current working directory as the second argument.
+    fn parse_dot_from(&self, cwd: &Path) -> io::Result<Cow<Path>>;
 }
